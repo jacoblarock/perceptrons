fbc4219c78e8700734683da467ba0f1f05baeb3f
Jacob LaRock <jacoblarock@gmail.com>
2025-05-06 21:36:48 +0200

    channel-based feed-forward

--- go-perceptron/network/perceptron.go
+++ go-perceptron/network/perceptron.go
@@ -8,22 +8,22 @@ import (
 )
 
 type perceptron struct {
-	learn_rate float32
-	weights []float32
+	learn_rate float64
+	weights []float64
 }
 
 type layer struct { 
 	perceps []perceptron
-	outs []chan float32
+	outs []chan float64
 }
 
-func make_layer(in_size int, p_count int, learn_rate float32) layer {
+func make_layer(in_size int, p_count int, learn_rate float64) layer {
 	var l layer
 	l.perceps = make([]perceptron, p_count)
-	l.outs = make([]chan float32, p_count)
+	l.outs = make([]chan float64, p_count)
 	for i := 0; i < p_count; i++ {
 		var p perceptron
-		p.weights = make([]float32, in_size)
+		p.weights = make([]float64, in_size)
 		p.learn_rate = learn_rate
 		l.perceps[i] = p
 	}
@@ -34,13 +34,13 @@ func net_rand_init(net []layer) {
 	for i := 0; i < len(net); i++ {
 		for j := 0; j < len(net[i].perceps); j++ {
 			for w := 0; w < len(net[i].perceps[j].weights); w++ {
-				net[i].perceps[j].weights[w] = rand.Float32()
+				net[i].perceps[j].weights[w] = rand.Float64()
 			}
 		}
 	}
 }
 
-func array_to_string(a []float32, delim string) string {
+func array_to_string(a []float64, delim string) string {
 	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
 }
 
@@ -53,16 +53,33 @@ func print_net(net []layer) {
 	}
 }
 
-func relu(val float32) float32 {
-	if val > 0 {
-		return val
+func relu(x float64) float64 {
+	if x > 0 {
+		return x
 	} else {
 		return 0
 	}
 }
 
-func eval_neuron(p perceptron, out chan float32, vals []float32) {
-	var sum float32
+func ddx_relu(x float64) float64 {
+	if x > 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func mse(y []float64, pred []float64) float64 {
+	sum := 0.0
+	for i := 0; i < len(y); i++ {
+		diff := y[i] - pred[i]
+		sum += diff * diff
+	}
+	return sum / float64(len(y))
+}
+
+func eval_neuron(p perceptron, out chan float64, vals []float64) {
+	var sum float64
 	sum = 0
 	for i := 0; i < len(vals); i++ {
 		sum += p.weights[i] * vals[i]
@@ -70,21 +87,23 @@ func eval_neuron(p perceptron, out chan float32, vals []float32) {
 	out <- relu(sum)
 }
 
-func eval_layer(l layer, vals []float32) {
+func eval_layer(l layer, vals []float64) {
 	for i := 0; i < len(l.perceps); i++ {
+		out := make(chan float64, 1)
+		l.outs[i] = out
 		go eval_neuron(l.perceps[i], l.outs[i], vals)
 	}
 }
 
-func await_outputs(outs []chan float32) []float32 {
-	out_floats := make([]float32, len(outs))
+func await_outputs(outs []chan float64) []float64 {
+	out_floats := make([]float64, len(outs))
 	for i := 0; i < len(outs); i++ {
 		out_floats[i] = <-outs[i]
 	}
 	return out_floats
 }
 
-func eval_net(layers []layer, inputs []float32) []float32 {
+func eval_net(layers []layer, inputs []float64) []float64 {
 	eval_layer(layers[0], inputs)
 	if len(layers) == 1 {
 		return await_outputs(layers[0].outs)
@@ -95,3 +114,5 @@ func eval_net(layers []layer, inputs []float32) []float32 {
 	}
 	return await_outputs(layers[len(layers)-1].outs)
 }
+
+
