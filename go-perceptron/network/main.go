fad99323cf6d0d4463e4174663e5921ee3178f85
Jacob LaRock <jacoblarock@gmail.com>
2025-05-29 20:38:12 +0200

    additions to train networks

--- go-perceptron/network/main.go
+++ go-perceptron/network/main.go
@@ -5,16 +5,26 @@ import (
 )
 
 func main() {
-	net := make([]layer, 2)
-	net[0] = make_layer(6, 3, 0.5)
-	net[1] = make_layer(3, 1, 0.5)
-	fmt.Println(net[0].perceps[0].weights[0])
+	net := make([]layer, 3)
+	net[0] = make_layer(2, 2, 0.5)
+	net[1] = make_layer(2, 2, 0.5)
+	net[2] = make_layer(2, 1, 0.5)
 	net_rand_init(net)
 	print_net(net)
-	test := make([]float32, 6)
+	y := []float64 {0.69}
+	test := make([]float64, 2)
 	for i := 0; i < len(test); i++ {
 		test[i] = 0.1
 	}
 	res := eval_net(net, test)
-	fmt.Println(res)
+	for i := 0; i < len(res); i++ {
+		fmt.Println(*res[i])
+	}
+	for i := 0; i < 100; i++ {
+		update_net_weights(net, res, y)
+		res = eval_net(net, test)
+		for i := 0; i < len(res); i++ {
+			fmt.Println(*res[i])
+		}
+	}
 }
--- go-perceptron/network/perceptron.go
+++ go-perceptron/network/perceptron.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"math/rand"
 	"fmt"
 	"strings"
@@ -69,6 +70,13 @@ func ddx_relu(x float64) float64 {
 	}
 }
 
+func sigmoid(x float64) float64 {
+	return 1.0 / (1.0 + math.Exp(-x))
+}
+func ddx_sigmoid(x float64) float64 {
+	return sigmoid(x) * (1 - sigmoid(x))
+}
+
 func mse(y []float64, pred []float64) float64 {
 	sum := 0.0
 	for i := 0; i < len(y); i++ {
@@ -84,7 +92,7 @@ func eval_neuron(p perceptron, out chan float64, vals []float64) {
 	for i := 0; i < len(vals); i++ {
 		sum += p.weights[i] * vals[i]
 	}
-	out <- relu(sum)
+	out <- sigmoid(sum)
 }
 
 func eval_layer(l layer, vals []float64) {
@@ -103,16 +111,76 @@ func await_outputs(outs []chan float64) []float64 {
 	return out_floats
 }
 
-func eval_net(layers []layer, inputs []float64) []float64 {
+func eval_net(layers []layer, inputs []float64) []*[]float64 {
+	outs := make([]*[]float64, len(layers))
 	eval_layer(layers[0], inputs)
-	if len(layers) == 1 {
-		return await_outputs(layers[0].outs)
-	}
 	for i := 1; i < len(layers); i++ {
 		layer_ins := await_outputs(layers[i-1].outs)
+		outs[i-1] = &layer_ins
 		eval_layer(layers[i], layer_ins)
 	}
-	return await_outputs(layers[len(layers)-1].outs)
+	net_out := await_outputs(layers[len(layers)-1].outs)
+	outs[len(outs)-1] = &net_out
+	return outs
 }
 
+func output_weights(layers []layer, layer_index int, p_index int) []float64 {
+	if layer_index == len(layers) - 1 {
+		out := make([]float64, 1)
+		out[0] = 1.0
+		return out
+	}
+	out_count := len(layers[layer_index + 1].outs)
+	out := make([]float64, out_count)
+	for i := 0; i < out_count; i++ {
+		out[i] = layers[layer_index + 1].perceps[i].weights[p_index]
+	}
+	return out
+}
 
+func unit_error(pred float64, out_weights []float64, next_errs []float64) float64 {
+	if len(out_weights) != len(next_errs) {
+		fmt.Print("weights and next error sizes should match!")
+	}
+	out := 0.0
+	for i := 0; i < len(out_weights); i++ {
+		out += ddx_sigmoid(pred) * out_weights[i] * next_errs[i]
+	}
+	return out
+}
+
+func weight_delta(learn_rate float64, unit_error float64, unit_out float64) float64 {
+	return learn_rate * unit_error * unit_out
+}
+
+func update_layer_weights(layers []layer, layer_index int, next_errs []float64, pred []float64) {
+	if layer_index >= len(layers) - 1 {
+		return
+	}
+	for i := 0; i < len(layers[layer_index].perceps); i++ {
+		out_weights := output_weights(layers, layer_index, i)
+		unit_err := unit_error(pred[i], out_weights, next_errs)
+		for j := 0; j < len(out_weights); j++ {
+			delta := weight_delta(layers[layer_index].perceps[i].learn_rate, unit_err, pred[i])
+			layers[layer_index + 1].perceps[j].weights[i] += delta
+		}
+	}
+}
+
+func update_net_weights(layers []layer, outs []*[]float64, y []float64) {
+	last_err := make([]float64, len(*outs[len(outs) - 1]))
+	for i := 0; i < len(y); i++ {
+		last_err[i] = y[i] * y[i]
+	}
+	next_errs := &last_err
+	for i := len(layers) - 1; i >= 0; i-- {
+		update_layer_weights(layers, i, *next_errs, *outs[i])
+		unit_errs := make([]float64, len(layers[i].perceps))
+		for j := 0; j < len(layers[i].perceps); j++ {
+			pred := (*outs[i])[j]
+			out_weights := output_weights(layers, i, j)
+			unit_errs[j] = unit_error(pred, out_weights, *next_errs)
+		}
+		next_errs = &unit_errs
+	}
+}
